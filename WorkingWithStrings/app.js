25dd841e2b9501b7e342f096223f0ef780e4acd8
JUan Tapia <tapiajuan9@gmail.com>
2024-01-11 18:22:36 -0500

    Working with strings

---
+++ WorkingWithStrings/app.js
@@ -0,0 +1,10 @@
+// Template Literals
+let x;
+const profesion='Machinist';
+const empresa='Paragon Medical';
+// x='Hola, trabajo como '+''+profesion+' at '+empresa+'.';
+x=`Hola, trabajo como ${profesion} at ${empresa} por mas de ${2+8} anos`
+// In the template literals, we can put variables and also, we can make operations.
+
+
+console.log(x);
