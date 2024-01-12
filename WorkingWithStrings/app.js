1488c04655ea42e6c14ee4b9b2fccbdc84f615dc
JUan Tapia <tapiajuan9@gmail.com>
2024-01-12 13:06:33 -0500

    WorkingWithStrings 1

--- WorkingWithStrings/app.js
+++ WorkingWithStrings/app.js
@@ -20,7 +20,15 @@ x1=x1.length;
 console.log(x1);             // 11
 
 let x2;
+let x3;
 x2=new String('Hola a todos');
 console.log(x2[0],x2[1],x2[2]); // H o l
 console.log(x2.__proto__); // shows a list
+x3=x2.substring(1,6);     // ola a
+x3=x2.substring(5);     // a todos, index 5 to the rest
+// substring and slice are similar but slice can go positive or negative
+x3=x2.substring(0,8); // output: Hola a t  [0,8> 0 is included but not 8
+x3=x2.slice(0,8); // output: Hola a t  [0,8> 0 is included but not 8
+
+console.log(x3);
 
