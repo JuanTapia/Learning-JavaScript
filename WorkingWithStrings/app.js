d82dd88018433ef65dbbc8e41975af585c8372c5
Juan Tapia <tapiajuan9@gmail.com>
2024-01-13 20:58:12 -0500

    Working with strings

--- WorkingWithStrings/app.js
+++ WorkingWithStrings/app.js
@@ -21,14 +21,27 @@ console.log(x1);             // 11
 
 let x2;
 let x3;
+// The negative numbers start From right to left, so S=0, O=-1, d=-2 and so on.
+// Hola a todos
+// 0123456789...
 x2=new String('Hola a todos');
 console.log(x2[0],x2[1],x2[2]); // H o l
 console.log(x2.__proto__); // shows a list
-x3=x2.substring(1,6);     // ola a
-x3=x2.substring(5);     // a todos, index 5 to the rest
+x3=x2.substring(1,6);     // ola a [1,6>
+x3=x2.substring(5);     // a todos, index 5 to the rest [5,--->]
 // substring and slice are similar but slice can go positive or negative
 x3=x2.substring(0,8); // output: Hola a t  [0,8> 0 is included but not 8
 x3=x2.slice(0,8); // output: Hola a t  [0,8> 0 is included but not 8
+x3=x2.slice(8); // output: odos [8,to the rest of the string toward right]
+x3=x2.slice(-9,-3); // output: a a to  <-9,-3]
+// x3=x2.substring(-9,-3);  Substrings dont take negative// substring and slice are similar but slice can go positive or negative
+x3=x2.trim();  // it gets rid of white space at the beginnig and end of the String, but not in between words. ('        Hola a todos        ');
+x3=x2.replace('a todos','Juan'); // output: Hola Juan
+x3=x2.includes('todos');         // output: true
+x3=x2.valueOf();         // it gives the primitive value of the string, even tho it's a object.
+x3=x2.split();          //Output: ['Hola a todos'] since we have not put anything. it gives an array with one element(the string)
+x3=x2.split('');   // ['H', 'o', 'l', 'a', ' ', 'a', ' ', 't', 'o', 'd', 'o', 's'] it gives an array which elemens are every character of the strings.
+x3=x2.split(' ');  // ['Hola', 'a', 'todos']
 
-console.log(x3);
+console.log(x3);  
 
