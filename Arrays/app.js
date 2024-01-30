53e082d940192c0bf7c7b800502c9aac15015870
Juan Tapia <tapiajuan9@gmail.com>
2024-01-29 19:46:47 -0500

    Arrays 0

---
+++ Arrays/app.js
@@ -0,0 +1,7 @@
+let x;
+const arreglo=new Array(12,13,56,1,3);
+const fruit=['Apple','Pear','Banana','Grapes'];
+
+x=arreglo[1]+arreglo[4];
+x=`Mi frutas favorita es ${fruit[2]} y ${fruit[3]}`;
+console.log(x);
